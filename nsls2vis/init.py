e262e0c328dbc7837fb4d738d33b2ed7e6dd9722
Eric Dill <edill@bnl.gov>
2014-07-06 17:02:51 -0400

    ENH: Integrated new vistools widgets into VisTrails

--- nsls2vis/init.py
+++ nsls2vis/init.py
@@ -8,17 +8,83 @@ from vistrails.core.modules.vistrails_module import Module
 from vistrails.core.modules.config import IPort, OPort
 from vistrails.packages.spreadsheet.basic_widgets import SpreadsheetCell
 from vistrails.packages.spreadsheet.spreadsheet_cell import QCellWidget
-from vistools.qt_widgets import StackScanner
-from vistools.qt_widgets import MainWindow
+from vistools.qt_widgets import CrossSectionMainWindow, Stack1DMainWindow
+import numpy as np
 
 
-class VisCell(SpreadsheetCell):
+class DataGen(Module):
+    _input_ports = [
+        IPort(name="num_datasets", label="Number of datasets to generate",
+              signature="basic:Integer"),
+    ]
+
+    _output_ports = [
+        OPort(name="OneDimStack", signature="basic:List"),
+        OPort(name="TwoDimStack", signature="basic:List"),
+        OPort(name="DataLabels", signature="basic:List"),
+    ]
+
+    def compute(self):
+        length = self.get_input("num_datasets")
+        self.set_output("OneDimStack", self.make_onedim(length))
+        self.set_output("TwoDimStack", self.make_twodim(length))
+        self.set_output("DataLabels", self.make_labels(length))
+
+    def make_onedim(self, length):
+        """
+        Construct a one dimensional stack of height 'length'
+
+        Parameters
+        ----------
+        length : int
+            number of datasets to generate
+
+        Returns
+        -------
+        data : list
+            list of 2d np.ndarray
+        """
+        x_axis = np.arange(0, 25, .01)
+        data = []
+        for idx in range(length):
+            x = x_axis
+            y = np.sin(x_axis + idx)
+            data.append((x, y))
+
+        return data
+
+    def make_twodim(self, length):
+        """
+        Construct a two dimensional stack of height 'length'
+
+        Parameters
+        ----------
+        length : int
+            number of datasets to generate
+
+        Returns
+        -------
+        data : list
+            list of 2d np.ndarray
+        """
+        x, y = [_ * 2 * np.pi / 500 for _ in np.ogrid[-500:500, -500:500]]
+        rep = int(np.sqrt(length))
+        data = []
+        for idx in range(length):
+            kx = idx // rep + 1
+            ky = idx % rep
+            data.append(np.sin(kx * x) * np.cos(ky * y) + 1.05)
+
+        return data
+
+    def make_labels(self, length):
+        return [str(_) for _ in range(length)]
+
+
+class CrossSectionCell(SpreadsheetCell):
     _input_ports = [
         IPort(name="data", label="Data to display",signature="basic:List"),
-        IPort(name="type", label="Data view to display",
-              signature="basic:String", entry_type="enum",
-              values=[str(MainWindow.messenger_classes[0]),
-                      str(MainWindow.messenger_classes[1])]),
+        IPort(name="keys", label="Names of the data",signature="basic:List"),
     ]
 
     _output_ports = [
@@ -27,77 +93,60 @@ class VisCell(SpreadsheetCell):
 
     def compute(self):
         data = self.get_input("data")
-        type = self.get_input("type")
-        self._widg = self.displayAndWait(VisCellWidget, (data,type,))
+        keys = self.get_input("keys")
+        self.cellWidget = self.displayAndWait(CrossSectionWidget, (data,keys,))
 
 
-class VisCellWidget(QCellWidget):
+class CrossSectionWidget(QCellWidget):
 
     def __init__(self, parent=None):
-        super(VisCellWidget, self).__init__(parent=parent)
+        super(CrossSectionWidget, self).__init__(parent=parent)
 
-    def update_contents(self, input_ports):
-        (data,type) = input_ports
+    def updateContents(self, input_ports):
+        (data,keys) = input_ports
         layout = QtGui.QHBoxLayout()
-        widg = MainWindow(messenger_class=type, data_list=data)
+        widg = CrossSectionMainWindow(data_list=data, key_list=keys)
         layout.addWidget(widg)
         self.setLayout(layout)
         QCellWidget.updateContents(self, input_ports)
 
-class StackScannerCell(SpreadsheetCell):
+
+class Stack1DCell(SpreadsheetCell):
     _input_ports = [
-        IPort(name="img_stack", label="Stack of images to display", \
-              signature="basic:List"),
-        ]
-    
+        IPort(name="data", label="Data to display",signature="basic:List"),
+        IPort(name="keys", label="Names of the data",signature="basic:List"),
+    ]
+
     _output_ports = [
-        OPort(name="displayed_image", signature="basic:List"),
-        ]
-    
+        OPort(name="displayed_data", signature="basic:List"),
+    ]
+
     def compute(self):
-        img_stack = self.get_input("img_stack")
-        self.cellWidget = self.displayAndWait(StackScannerCellWidget, (img_stack,))
-    
-    def set_out_arr(self, arr_2d):
-        self.set_output("displayed_image", arr_2d)
+        data = self.get_input("data")
+        print("got data")
+        keys = self.get_input("keys")
+        print("got the keys")
+        self.cellWidget = self.displayAndWait(Stack1DWidget, (data,keys,))
 
 
-class StackScannerCellWidget(QCellWidget):
-    
+class Stack1DWidget(QCellWidget):
+
     def __init__(self, parent=None):
-        QCellWidget.__init__(self,parent)
-        self._stack_scanner = None
-        
-    def updateContents(self, inputPorts):
-        """ updateContents(inputPorts: tuple) -> None
-        Update the widget contents based on the input data
-        
-        """
-        (img_stack, ) = inputPorts
-        if img_stack is not None:
-            if self._stack_scanner is None:
-                self._stack_scanner = StackScanner(img_stack)
-                hbox = QtGui.QHBoxLayout()
-                hbox.addWidget(self._stack_scanner)
-                self.setLayout(hbox)
-            else:
-                self._stack_scanner.set_img_stack(img_stack)
-                
-        else:
-            self.label.setText("Invalid image stack file!")
+        super(Stack1DWidget, self).__init__(parent=parent)
 
-        QCellWidget.updateContents(self, inputPorts)
-    
-    #===========================================================================
-    # def clearLayout(self, layout):
-    #     if layout is not None:
-    #         while layout.count():
-    #             item = layout.takeAt(0)
-    #             widget = item.widget()
-    #             if widget is not None:
-    #                 widget.deleteLater()
-    #             else:
-    #                 self.clearLayout(item.layout())
-    #===========================================================================
+    def updateContents(self, input_ports):
+        (data,keys) = input_ports
+        print("got data and keys")
+        layout = QtGui.QHBoxLayout()
+        print("declared the layout")
+        widg = Stack1DMainWindow(data_list=data, key_list=keys)
+        print("declared the stack1Dmainwindow")
+        layout.addWidget(widg)
+        print("added the widget to the layout")
+        self.setLayout(layout)
+        print("set the layout")
+        QCellWidget.updateContents(self, input_ports)
 
-_modules = [StackScannerCell]
+
+
+_modules = [CrossSectionCell, Stack1DCell, DataGen]
